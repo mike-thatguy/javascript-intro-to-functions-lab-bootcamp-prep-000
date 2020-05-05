df609275ed6206c2f855fb708415c62be4217063
Mike Gretton <mikegretton@gmail.com>
2020-05-05 22:04:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === "I love you, Grandma.") {
     return "I love you, too.";
-  } elseif (string === string.toLowerCase()) {
+  } else if (string === string.toLowerCase()) {
     return "I can't hear you!";
-  } elseif (string === string.toUpperCase()) {
+  } else if (string === string.toUpperCase()) {
     return "YES INDEED!";
   }
 }
